0f181385f823170a5c9f7282f9a851a82f618166
roynaldi3301 <prasbhara0604@gmail.com>
2023-09-03 20:33:37 +0700

    tambah fitur bot

---
+++ commands/game/tebak/susunkata.js
@@ -0,0 +1,39 @@
+const users = require('@database/services/users')
+const i18n = require('i18n')
+const api = require('@libs/utils/api')
+
+const _collection = new Map()
+
+/**
+ * @type { import('@libs/builders/command').ICommand }
+ */
+module.exports = {
+    category: 'game',
+    description: 'Game asah otak, guest and get exp.',
+    callback: async ({ msg }) => {
+        if (_collection.get(msg.from)) {
+            return msg.reply(i18n.__('game.finish_last_first'), _collection.get(msg.from))
+        }
+
+        const { data } = await api('lolhuman').get('/api/tebak/susunkata').catch(console.error)
+        let question = await msg.reply(`${data.result.pertanyaan}\n\nWaktu 60 Detik`.format({ question: data.result.pertanyaan }))
+
+        _collection.set(msg.from, question)
+
+        msg.createMessageCollector({
+            filter: data.result.jawaban.toLowerCase(),
+            max: 1,
+        })
+            .on('collect', (msg) => {
+                let xp = Math.floor(Math.random() * (999 - 1) + 1)
+                users.addExp(msg, msg.senderNumber, xp)
+                msg.reply(i18n.__('game.right_answer', { xp }))
+            })
+            .on('end', (res) => {
+                _collection.delete(msg.from)
+                if (res == 'timeout') {
+                    msg.reply(i18n.__('game.timeout_answer', { answer: data.result.jawaban }), question)
+                }
+            })
+    },
+}
